f237a5660fbcb7152aad6e20e274b68bdf34daad
Lawrence <lawrence@acode.ninja>
2018-03-15 10:08:50 +0000

    Adds translations loading and validator replacer

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,7 +14,19 @@ class ServiceProvider extends IlluminateServiceProvider
      */
     public function boot()
     {
+        // Register translations for the validator
+        $this->loadTranslationsFrom(__DIR__.'/translations', 'pwned_validator');
+
+        $this->publishes([
+            __DIR__.'/translations' => resource_path('lang/vendor/pwned_validator'),
+        ]);
+
         // Extend the laravel validator factory with the validator
         Validator::extend('pwned_password_strict', '\ACodeNinja\PwnedPasswordsValidator\Validator@validateStrict');
+
+        // Add the replacer for the pwned_password_strict validator
+        Validator::replacer('pwned_password_strict', function ($message, $attribute, $rule, $parameters) {
+            return trans('pwned_validator::validation.pwned_password_strict');
+        });
     }
 }
