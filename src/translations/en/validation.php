cbf370f9d5675d3883a2c2fac01a25044110e20d
Lawrence <lawrence@acode.ninja>
2018-03-15 10:07:13 +0000

    Adds package translations

---
+++ src/translations/en/validation.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'pwned_password_strict' => 'The given password was found in an online database of passwords found in data breaches. It is inadvisable to use this as your password.'
+];
